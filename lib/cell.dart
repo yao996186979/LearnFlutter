036a2d6462565db7d13f600bea5316590196bcce
yao1248170343 <yao996186979@gmail.com>
2019-09-03 18:01:06 +0800

    自定义类iOS cell

--- lib/cell.dart
+++ lib/cell.dart
@@ -21,3 +21,43 @@ class CustomCell extends StatelessWidget {
     );
   }
 }
+
+class NavigationCell extends StatelessWidget {
+  // 类似iOS中init初始化方法的重新
+  const NavigationCell({
+    Key key,
+    this.labelTitle: "默认标题",
+    this.detailLabelTitle: "默认尾部标题",
+  }) : super(key: key);
+
+  //属性声明
+  final String labelTitle;
+  final String detailLabelTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Row(children: <Widget>[
+            new Padding(
+                padding: new EdgeInsets.only(left: 15),
+                child: new Text(this.labelTitle,
+                    style: new TextStyle(color: Colors.black, fontSize: 24))),
+            new Padding(
+                padding: new EdgeInsets.only(right: 15),
+                child: new Row(
+                  children: <Widget>[
+                    new Text(this.detailLabelTitle,
+                        style: new TextStyle(color: Colors.grey, fontSize: 16)),
+                    new Icon(IconData(0xebc7, fontFamily: 'IconFont'), size: 16)
+                  ],
+                ))
+          ], mainAxisAlignment: MainAxisAlignment.spaceBetween),
+          new Divider()
+        ],
+      ),
+      height: 50,
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'cell.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -7,11 +8,10 @@ class HomePage extends StatelessWidget {
       appBar: new AppBar(
         title: new Text('Home Page'),
       ),
-      body: new Center(
-          child: new ListView(children: getCustomData())
-      ),
+      body: new Center(child: new ListView(children: getNavigationData(context))),
     );
   }
+
   getTextData() {
     List<Widget> widgets = [];
     for (int i = 0; i < 100; i++) {
@@ -19,10 +19,30 @@ class HomePage extends StatelessWidget {
     }
     return widgets;
   }
+
   getCustomData() {
     List<Widget> widgets = [];
     for (int i = 0; i < 20; i++) {
-      widgets.add(new CustomCell(color: i%2==0?Colors.blueGrey:Colors.amber,child: Text("${i}")));
+      widgets.add(new CustomCell(
+          color: i % 2 == 0 ? Colors.blueGrey : Colors.amber,
+          child: Text("${i}")));
+    }
+    return widgets;
+  }
+
+  getNavigationData(BuildContext context) {
+    List<String> titles = ["导航", "列表"];
+    List<Widget> widgets = [];
+    for (int i = 0; i < titles.length; i++) {
+      widgets.add(new GestureDetector(
+        child: new NavigationCell(
+          labelTitle: titles[i],
+          detailLabelTitle: "${i}",
+        ),
+        onTap: (){
+          print("ddd");
+        },
+      ));
     }
     return widgets;
   }
