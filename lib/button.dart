9d11cb9288812aeb1e8d1fafa9dbf5c2c0412e96
yao1248170343 <yao996186979@gmail.com>
2019-10-09 18:44:17 +0800

    按钮持续学习中。。

--- lib/button.dart
+++ lib/button.dart
@@ -1,22 +1,22 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ButtonPage extends StatelessWidget{
+class ButtonPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     // TODO: implement build
     return new Scaffold(
       appBar: new AppBar(title: new Text("Button")),
       body: new Column(children: loadButton()),
     );
   }
+
   //    new ButtonBar();
 //    new ButtonTheme();
 //    new DropdownButton(items: null, onChanged: null);
 //    new CloseButton();
 //    new BackButton();
+//    new BackButtonIcon();
 //    new FlatButton(onPressed: null, child: null);
 //    new FloatingActionButton(onPressed: null);
 //    new IconButton(icon: null, onPressed: null);
@@ -28,46 +28,49 @@ class ButtonPage extends StatelessWidget{
 //    new DropdownButtonFormField(items: null);
 //    new DropdownButtonHideUnderline(child: null);
 //    new ToggleButtons(children: null, isSelected: null);
-//    new BackButtonIcon();
 //    new FlatButton.icon(onPressed: null, icon: null, label: null);
 //    new FloatingActionButton.extended(onPressed: null, label: null);
 //    new FloatingActionButtonAnimator();
 //    new FloatingActionButtonThemeData();
 //    new CupertinoButton(child: null, onPressed: null);
 //    new CupertinoButton.filled(child: null, onPressed: null);
-  loadButton(){
+  loadButton() {
     String selectedValue = "1";
     List<Widget> buttons = new List();
 
     // ButtonBar
-    ButtonBar buttonBar = new ButtonBar(children: <Widget>[
-      new Text("111"),
-      new Text("222"),
-      new Text("333")
-      ],alignment: MainAxisAlignment.spaceAround,mainAxisSize: MainAxisSize.max);
+    ButtonBar buttonBar = new ButtonBar(
+        children: <Widget>[new Text("111"), new Text("222"), new Text("333")],
+        alignment: MainAxisAlignment.spaceAround,
+        mainAxisSize: MainAxisSize.max);
     buttons.add(buttonBar);
 
     // ButtonTheme
-    ButtonTheme buttonTheme =  new ButtonTheme(
+    ButtonTheme buttonTheme = new ButtonTheme(
       textTheme: ButtonTextTheme.normal,
-      child: new Container(child: Text("123"),color: Colors.red,),
+      child: new Container(
+        child: Text("测试ButtonTheme"),
+        color: Colors.red,
+      ),
       buttonColor: Colors.amber,
-      focusColor: Colors.black38,
+      focusColor: Colors.white,
       highlightColor: Colors.blue,
       hoverColor: Colors.deepPurpleAccent,
       height: 50,
       disabledColor: Colors.red,
-      alignedDropdown: true,
+      alignedDropdown: false,
       minWidth: 1000,
     );
     buttons.add(buttonTheme);
 
     // BackButton 返回按钮 默认返回上一页
-    BackButton backButton = new BackButton(color: Colors.red,);
+    BackButton backButton = new BackButton(
+      color: Colors.red,
+    );
     buttons.add(backButton);
 
     // BackButtonIcon 返回按钮的icon
-    BackButtonIcon backButtonIcon =  new BackButtonIcon();
+    BackButtonIcon backButtonIcon = new BackButtonIcon();
     buttons.add(backButtonIcon);
 
     // CloseButton 关闭按钮 默认关闭当前页面
@@ -75,19 +78,73 @@ class ButtonPage extends StatelessWidget{
     buttons.add(closeButton);
 
     // DropdownButton
-    DropdownButton dropdownButton = new DropdownButton(items: [
-      new DropdownMenuItem(child: Text("111"),value: "1",),
-      new DropdownMenuItem(child: Text("222"),value: "2",),
-      new DropdownMenuItem(child: Text("333"),value: "3",)
-    ]
-    , onChanged: (T){//下拉菜单item点击之后的回调
-
-        },hint: Text("请选择"),value: selectedValue,);
+    DropdownButton dropdownButton = new DropdownButton(
+      items: [
+        new DropdownMenuItem(
+          child: Text("111"),
+          value: "1",
+        ),
+        new DropdownMenuItem(
+          child: Text("222"),
+          value: "2",
+        ),
+        new DropdownMenuItem(
+          child: Text("333"),
+          value: "3",
+        )
+      ],
+      onChanged: (T) {
+        //下拉菜单item点击之后的回调
+        print(T);
+//      setState(() {               //StatefulWidget中才可以刷新状态
+//        selectedValue=T;
+//      });
+      },
+      hint: Text("请选择"),
+      value: selectedValue,
+      iconSize: 40,
+      isDense: false,
+      isExpanded: true,
+      iconEnabledColor: Colors.red,
+      iconDisabledColor: Colors.deepPurpleAccent,
+      underline: new Padding(
+          padding: EdgeInsets.all(8.0),
+          child: new Divider(
+            color: Colors.red,
+          )),
+      elevation: 20,
+    );
     buttons.add(dropdownButton);
+
+    MaterialButton materialButton = new MaterialButton(onPressed: (){},
+      color: Colors.yellow,
+      focusColor: Colors.red,
+      hoverColor: Colors.blue,
+      textColor: Colors.deepPurple,
+      highlightColor: Colors.white,
+        splashColor:Colors.red,
+        colorBrightness:Brightness.dark,
+      child: Text("materialButton"),);
+    buttons.add(materialButton);
+
+    OutlineButton outlineButton = new OutlineButton(onPressed: null,child: Text("outlineButton"),);
+    buttons.add(outlineButton);
+    
+    FlatButton flatButton = new FlatButton(onPressed: (){
+    }, child: Text("flatButton"));
+    buttons.add(flatButton);
+
+    RaisedButton raisedButton = new RaisedButton(onPressed: null,child: Text("raisedButton"),);
+    buttons.add(raisedButton);
+
+    FloatingActionButton floatingActionButton = new FloatingActionButton(onPressed: (){},child: Text("点"),);
+    buttons.add(floatingActionButton);
+
+    IconButton iconButton = new IconButton(icon: new Icon(IconData(0xebc7, fontFamily: 'IconFont'), size: 16), onPressed: (){});
+    buttons.add(iconButton);
+
+
+
     return buttons;
-
-
   }
-
-
 }
